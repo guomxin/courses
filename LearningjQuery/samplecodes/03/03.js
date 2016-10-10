96219f40f930aeefb77dd0a97209c03635177221
guomxin <guomaoxin@foxmail.com>
2016-10-10 10:54:13 +0800

    0.8 version LearningjQuery ch3 excercise

--- LearningjQuery/samplecodes/03/03.js
+++ LearningjQuery/samplecodes/03/03.js
@@ -4,3 +4,40 @@
 // page or follow along with the examples in the book.
 //
 // See README.txt for more information.
+$(document).ready(function(){
+    $("div.author").click(function(){
+       $(this).addClass("selected"); 
+    });
+    
+    $("h3.chapter-title").dblclick(function(){
+        $("#chapter-1 > p").toggleClass("hidden");
+    });
+    
+    var setBodyClass = function(className) {
+        $('body').removeClass().addClass(className);
+    };
+    var curStyleIndex = 0;
+    var styles = ["default", "narrow", "large"];
+    $(document).keyup(function(event) {
+        if (event.which == 39) {
+            curStyleIndex += 1;
+            if (curStyleIndex == 3) curStyleIndex = 0;
+            setBodyClass(styles[curStyleIndex]);
+        }
+    });
+    
+    $(document).mousemove(function(event) {
+        // console.log("X:" + event.pageX + ", Y:" + event.pageY);
+    });
+    
+    var curObj = null;
+    $(document).on('mousedown', 'p', function(event) {
+        $(this).addClass('hidden'); 
+        curObj = $(this);
+        // console.log("mousedown" + $(this));
+    });
+    $(document).on('mouseup', function(event) {
+        curObj.removeClass('hidden');
+        // console.log("mouseup" + $(this));
+    });
+})
