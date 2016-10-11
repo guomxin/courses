d9e9ee4558ede45b431c0e1754d4b1d002566ce8
guomxin <guomaoxin@foxmail.com>
2016-10-11 10:52:18 +0800

    0.8 version LearningjQuery ch4 excercise

--- LearningjQuery/samplecodes/04/04.js
+++ LearningjQuery/samplecodes/04/04.js
@@ -4,3 +4,53 @@
 // page or follow along with the examples in the book.
 //
 // See README.txt for more information.
+$(document).ready(function() {
+    $('#container').fadeIn('slow'); 
+    
+    var initialBgColor = $('p').css("backgroundColor");
+    $('p').mouseover(function(event) {
+       $(this).css("backgroundColor", "yellow");
+    });
+    $('p').mouseout(function(event) {
+       $(this).css("backgroundColor", initialBgColor); 
+    });
+    
+    $('h2').click(function() {
+       $(this).animate({opacity:0.25, marginLeft: '20px'}, {
+           duration:'slow',
+           complete: function() {
+               $('div.speech').fadeTo('slow', 0.5);
+           }
+       }); 
+    });
+    
+    
+    $switcher = $('#switcher');
+    $switcher.css('position', 'relative');
+    var curLeft = parseInt($switcher.offset().left);
+    var curTop = parseInt($switcher.offset().top);
+    $(document).keyup(function(event) {
+        console.log("before: " + event.which + "," + curLeft + "," + curTop);
+        
+        switch (event.which) {
+            case 37: // left
+                curLeft -= 20;
+                console.log("After: " + event.which + "," + curLeft + "," + curTop);
+                $switcher.animate({left: curLeft}, 'slow');
+                break;
+            case 38: // up
+                curTop -= 20;
+                $switcher.animate({'top': curTop}, 'slow');
+                break;
+            case 39: // right
+                curLeft += 20;
+                $switcher.animate({left: curLeft}, 'slow');
+                break;
+            case 40: // down
+                curTop += 20;
+                $switcher.animate({'top': curTop}, 'slow');
+                break;
+            default:
+        }
+    });
+});
