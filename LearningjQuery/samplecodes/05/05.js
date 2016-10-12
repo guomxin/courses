e8c29ae63075f14a6880d686d46f88eaed26a589
guomxin <guomaoxin@foxmail.com>
2016-10-12 09:54:00 +0800

    0.8 version LearningjQuery ch5 excercise

--- LearningjQuery/samplecodes/05/05.js
+++ LearningjQuery/samplecodes/05/05.js
@@ -4,3 +4,25 @@
 // page or follow along with the examples in the book.
 //
 // See README.txt for more information.
+$(document).ready(function() {
+   $('<a id="top"></a>').prependTo('body');
+   $('div.chapter p').each(function() {
+       var $link = $('<a href="#top">back to top</a>').click(function() {
+           $(this).after('<p>You were here</p>');
+       });
+       $(this).after($link); 
+   });
+    
+    $('#f-author').click(function() {
+        if ($(this).html().indexOf("<b>") == -1) {
+            $(this).html('<b>' + $(this).text() + '</b>');     
+        } else {
+            $(this).html($(this).text());
+        }
+    });
+    
+    $('div.chapter p').each(function() {
+       var classValue = $(this).attr('class');
+       $(this).attr({class: classValue + " inhabitants"});
+    });
+});
