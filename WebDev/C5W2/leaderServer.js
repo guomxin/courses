13db8ce5f0711a2c4dacd90503d5b8742a2cc0fb
guomxin <guomaoxin@foxmail.com>
2016-11-02 15:34:57 +0800

    0.8 version WebDev course5 week2 assignment

---
+++ WebDev/C5W2/leaderServer.js
@@ -0,0 +1,41 @@
+var mongoose = require('mongoose'),
+    assert = require('assert');
+
+var Leaders = require('./models/leadership');
+
+// Connection URL
+var url = 'mongodb://localhost:27017/conFusion';
+mongoose.connect(url);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    // we're connected!
+    console.log("Connected correctly to server");
+
+    // create a new user
+    var newLeader = Leaders({
+       name: "Peter Pan",
+      image: "images/alberto.png",
+      designation: "Chief Epicurious Officer",
+      abbr: "CEO",
+      description: "Our CEO, Peter, . . ."
+    });
+
+    // save the user
+    newLeader.save(function (err, leader) {
+        if (err) throw err;
+        console.log('Leader created!');
+        var id = leader._id;
+
+        // get all the users
+        Leaders.find({_id:id}, function (err, leaders) {
+            if (err) throw err;
+
+            // object of all the users
+            console.log(leaders[0]);
+            db.collection('leaders').drop(function () {
+                db.close();
+            });
+        });
+    });
+});
